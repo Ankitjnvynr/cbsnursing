a22b31cfe74d26c822e2ce719aba21f26cba5482
ankitjnvynr <ankitbkana@outlook.com>
2025-03-06 13:26:29 +0530

    export added

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // Enables static export
+    images: {
+      unoptimized: true, // Required for static export if using next/image
+    },
+  };
+  
+  module.exports = nextConfig;
+  
